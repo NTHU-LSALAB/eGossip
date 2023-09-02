a84f1d61a6c7ca3d01d517fdea25b8cac6f2a3be
kerwenwwer <kerwintsai@pm.me>
2023-09-03 04:49:38 +0800

    Add TcHandler function.

--- cmd/bpf.go
+++ cmd/bpf.go
@@ -1,7 +1,25 @@
 package cmd
 
-import ()
+import (
+	bpf "github.com/kerwenwwer/xdp-gossip/bpf"
+	"github.com/vishvananda/netlink"
+)
 
-func xdpbind(nodeList *NodeList) {
+func TcHandler(nodeList *NodeList) {
+	// Load bpf objects
+	obj, err := bpf.LoadObjects()
+	if err != nil {
+		nodeList.println("[Error]:", "Failed to load objects: %v", err)
+	}
 
+	link, err := netlink.LinkByName(nodeList.localNode.LinkName)
+	if err != nil {
+		nodeList.println("[Error]:", "Failed to get link by name %v", err)
+	}
+
+	if err := bpf.AttachTC(obj, link); err != nil {
+		nodeList.println("[Error]:", "Failed to attach TC: %v", err)
+	}
+
+	nodeList.println("[Info]:", "TC attached")
 }
