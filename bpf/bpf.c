407b3e3ebd0700a5a1a655a26d6b1b6f65608370
kerwenwwer <kerwintsai@pm.me>
2023-10-29 15:22:39 +0800

    Delete useless print and format code.

--- bpf/bpf.c
+++ bpf/bpf.c
@@ -29,52 +29,53 @@ typedef __u32 u32;
 typedef __u16 u16;
 
 /* Node info struct for store node information. */
-struct node_info
-{
+struct node_info {
 	__u32 ip;
 	__u16 port;
 	char mac[ETH_ALEN];
 };
 
 /* Broadcast target struct, including node info and current count. */
-struct targets
-{
+struct targets {
 	struct node_info target_list[MAX_TARGETS];
 	__u16 max_count;
 };
 
 /* Metadat struct for store latest metadata. */
-struct metadata
-{
+struct metadata {
 	char metadata[MAX_SIZE];
-	__u64 update_time;
+	int64_t update_time;
 };
 
-
 /* Message struct for commucation between kerenlspace and userspace. */
-struct message
-{
+struct message {
 	char type;
 	struct node_info node;
 };
 
 /* BPF_MAP_TYPE_HASH for nodelist */
-struct
-{
+struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
 	__type(key, __u16);
 	__type(value, struct targets);
 	__uint(max_entries, 1024);
 } nodelist_map SEC(".maps"); 
 
-/* BPF_MAP_TYPE_HASH for metadata */
-struct
-{
+/* BPF_MAP_TYPE_HASH for broadcast target */
+struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
 	__type(key, __u16);
 	__type(value, struct targets);
 	__uint(max_entries, 1024);
 } targets_map SEC(".maps"); // map for targets
+
+/* BPF_MAP_TYPE_HASH for metadata store */
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, __u16);
+	__type(value, struct metadata);
+	__uint(max_entries, 1);
+} metadata_map SEC(".maps"); // map for metadata
 
 /* Compute ip checksum for cloned packet before TC_ACT_OK. */
 static __always_inline __u16 compute_ip_checksum(struct iphdr *ip)
@@ -84,8 +85,7 @@ static __always_inline __u16 compute_ip_checksum(struct iphdr *ip)
 
 	ip->check = 0;
 #pragma clang loop unroll(full)
-	for (int i = 0; i < (sizeof(*ip) >> 1); i++)
-	{
+	for (int i = 0; i < (sizeof(*ip) >> 1); i++) {
 		csum += *next_ip_u16++;
 	}
 
@@ -96,7 +96,7 @@ static __always_inline __u16 compute_ip_checksum(struct iphdr *ip)
 static __always_inline int type_checker(const char *payload)
 {
 	// Search for the key in the payload
-	if ( payload[0] != '{' ){
+	if ( payload[0] != '{' ) {
 		return -1;
 	} else {
 		if ( payload[2] == 'T' && payload[5] == 'e' && payload[7] == ':') {
@@ -115,8 +115,7 @@ static __always_inline int type_checker(const char *payload)
 /* Map key handler for checking broadcast target map key. */
 static __always_inline __u16 mapkey_checker(const char *payload)
 {
-	if (payload[21] != 'M' || payload[23] != 'p' || payload[26] != 'y')
-	{
+	if (payload[21] != 'M' || payload[23] != 'p' || payload[26] != 'y') {
 		return 1;
 	}
 
@@ -151,14 +150,12 @@ int fastbroadcast(struct __sk_buff *skb)
 		return TC_ACT_OK;
 
 	struct ethhdr *eth = data;
-	if (eth->h_proto != htons(ETH_P_IP))
-	{
+	if (eth->h_proto != htons(ETH_P_IP)) {
 		return TC_ACT_OK;
 	}
 
 	struct iphdr *ip = data + l3_off;
-	if (ip->protocol != IPPROTO_UDP)
-	{
+	if (ip->protocol != IPPROTO_UDP) {
 		return TC_ACT_OK;
 	}
 
@@ -168,26 +165,20 @@ int fastbroadcast(struct __sk_buff *skb)
 		return TC_ACT_OK;
 	}
 
-	if (payload + 40 >= data_end){
+	if (payload + 40 >= data_end) {
 		return TC_ACT_OK;
 	}
 
-	if (payload > data_end){
+	if (payload > data_end) { 
 		return TC_ACT_OK;
 	}
 
-	if (type_checker(payload) != 1)
-	{
-		// Valid packet but not broadcast packet
-#ifdef DEBUG
-		bpf_printk("[fastbroad_prog] is not a broadcast packet\n");
-#endif
-		return TC_ACT_OK;
+	if (type_checker(payload) != 1) {
+		return TC_ACT_OK; // Valid packet but not broadcast packet, allow it
 	}
-
 
 	__u16 key = mapkey_checker(payload);
-	if(key == 1){
+	if(key == 1) {
 #ifdef DEBUG_B1
 		if (payload+50 > data_end) return TC_ACT_OK;
 		bpf_printk("[fastbroad_prog] key == 1, type=%d", type_checker(payload));
@@ -198,8 +189,7 @@ int fastbroadcast(struct __sk_buff *skb)
 		return TC_ACT_OK;
 	}
 	struct targets *tgt_list = bpf_map_lookup_elem(&targets_map, &key);
-	if (!tgt_list)
-	{
+	if (!tgt_list) {
 #ifdef DEBUG_SEND
 		__u8 b1, b2, b3, b4;
 		__u8 c1, c2, c3, c4;
@@ -212,12 +202,11 @@ int fastbroadcast(struct __sk_buff *skb)
 
 
 	/* Clone packet if curr < max_count */
-
 	char nxt;
 	u16 curr = payload[18];
 	char curr_char = payload[18];
-	if (curr < tgt_list->max_count)
-	{
+
+	if (curr < tgt_list->max_count) {
 		nxt = payload[18] + 1;
 		payload[18] = nxt;
 #ifdef DEBUG_SEND
@@ -244,43 +233,32 @@ int fastbroadcast(struct __sk_buff *skb)
 	if (payload + sizeof(__u64) > data_end)
 		return TC_ACT_OK;
 
-	if (payload + 45 >= data_end)
-	{
-#ifdef DEBUG_SEND
-		bpf_printk("[fastbroad_prog] TC_ACT_SHOT (type_str + 45 >= data_end)\n");
-#endif
+	if (payload + 45 >= data_end) {
 		return TC_ACT_SHOT;
 	}
 
-	if (payload > data_end)
-	{
-#ifdef DEBUG_SEND
-		bpf_printk("[fastbroad_prog] TC_ACT_SHOT (payload > data_end)\n");
-#endif
+	if (payload > data_end) {
 		return TC_ACT_SHOT;
 	}
 
-	if (tgt_list->max_count - '0' < tgt_list->max_count - curr){
+	if (tgt_list->max_count - '0' < tgt_list->max_count - curr) {
 #ifdef DEBUG_SEND
 		bpf_printk("[fastbroad_prog] TC_ACT_SHOT (Counting error)\n");
 #endif
 		return TC_ACT_SHOT;
 	}
+
 	int num = (tgt_list->max_count - '0') - (tgt_list->max_count - curr);
 
 #ifdef DEBUG_SEND
 	bpf_printk("[fastbroad_prog] egress packet: num:%d, curr: %d, max_count:%d\n", num, payload[18] - '0', tgt_list->max_count - '0');
 #endif
 
-	if (num < 0 || num >= MAX_TARGETS)
-	{
-#ifdef DEBUG_SEND
-		bpf_printk("[fastbroad_prog] TC_ACT_SHOT (num<0 or num>=MAX_TARGETS)\n");
-#endif
+	if (num < 0 || num >= MAX_TARGETS) {
 		return TC_ACT_SHOT;
 	}
 
-	if (tgt_list->target_list[num].ip == 0 || tgt_list->target_list[num].port == 0){
+	if (tgt_list->target_list[num].ip == 0 || tgt_list->target_list[num].port == 0) {
 #ifdef DEBUG_SEND
 		__u8 b1, b2, b3, b4;
 		ip_to_bytes(ip->saddr, &b1, &b2, &b3, &b4);
@@ -289,6 +267,7 @@ int fastbroadcast(struct __sk_buff *skb)
 		return TC_ACT_OK;
 	}
 
+	/* Update cloned packet content */
 	udp->dest = htons(tgt_list->target_list[num].port);
 	udp->check = 0;
 	ip->daddr = tgt_list->target_list[num].ip;
@@ -319,38 +298,27 @@ int fastdrop(struct xdp_md *ctx)
 		return XDP_PASS;
 
 	struct ethhdr *eth = data;
-	if (eth->h_proto != htons(ETH_P_IP))
-	{
+	if (eth->h_proto != htons(ETH_P_IP)){
 		return XDP_PASS;
 	}
 
-	if (eth->h_proto != htons(ETH_P_IP))
-	{
+	if (eth->h_proto != htons(ETH_P_IP)){
 		return XDP_PASS; // Non-IP packet, allow it
 	}
 
 	struct iphdr *ip = (struct iphdr *)(eth + 1);
-	if (ip->protocol != IPPROTO_UDP)
-	{
+	if (ip->protocol != IPPROTO_UDP){
 		return XDP_PASS; // Non-UDP packet, allow it
 	}
 
 	struct udphdr *udp = (struct udphdr *)(ip + 1);
-	if (udp + 10 > data_end)
-	{
+	if (udp + 10 > data_end){
 		return XDP_DROP; // Malformed packet
 	}
 
 	unsigned char *payload = (unsigned char *)(udp + 1);
-	if (payload + 1 > data_end)
-	{
+	if (payload + 1 > data_end){
 		return XDP_DROP; // Malformed packet
-	}
-
-	// Packet too small to contain the key
-	if (type_checker(payload) <= 1)
-	{
-		return XDP_PASS;
 	}
 
 	if(payload + 40 > data_end){
@@ -361,7 +329,7 @@ int fastdrop(struct xdp_md *ctx)
 		return XDP_PASS; //Broadcast packet, allow it
 	}
 
-	
+
 
 
 	return XDP_PASS;
