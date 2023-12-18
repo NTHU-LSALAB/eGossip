69a23786f96fad170ef044d0a886584cad703772
kerwenwwer <kerwintsai@pm.me>
2023-12-18 14:00:21 +0000

    Update benchmark script

--- script/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -X POST -H "Content-Type: application/json" -d '{"test-meta-11"}' http://192.168.3.11:8000/publish
-sleep 1
-curl -X POST -H "Content-Type: application/json" -d '{"test-meta-12"}' http://192.168.3.11:8000/publish
-sleep 1
-curl -X POST -H "Content-Type: application/json" -d '{"test-meta-13"}' http://192.168.3.11:8000/publish
-sleep 1
-curl -X POST -H "Content-Type: application/json" -d '{"test-meta-14"}' http://192.168.3.11:8000/publish
--- script/test.py
+++ script/test.py
@@ -5,6 +5,7 @@ import string
 import multiprocessing
 import subprocess
 import json
+import argparse
 
 # Declare NODES and results as a global variable
 NODES = []
@@ -21,7 +22,7 @@ def execute_command(command):
 
 # Function to extract IP addresses from kubectl output
 def get_ip_addresses():
-    command = "kubectl get pods -n default -l app=gossip-service -o wide | awk '{if(NR>1) print $6}'"
+    command = "kubectl get pods -n gossip -l app=gossip-service -o wide | awk '{if(NR>1) print $6}'"
     output = execute_command(command)
     return output.splitlines()
 
@@ -49,6 +50,29 @@ def metadata_update(node_list):
             metadata = random_metadata_string()
             send_metadata_update(node, metadata)
             time.sleep(1/1500000000 * 60)  # Spread out the 150 updates over one minute
+
+def test_configurations():
+    # Retrieve the IP addresses
+    ip_addresses = get_ip_addresses()
+
+    for ip in ip_addresses:
+        # Construct the curl command
+        curl_command = f"curl http://{ip}:8000/list"
+        
+        try:
+            curl_output = execute_command(curl_command)
+
+            parsed_output = json.loads(curl_output)
+            desired_data = parsed_output[0]
+            desired_data_str = json.dumps(desired_data, indent=4)
+            #print(desired_data_str)
+
+            results.append((ip, desired_data_str))
+        except Exception as e:
+            print(f"Error accessing {ip}: {e}")
+
+        print(results)    
+
 
 # Function to configure the servers
 def configure_servers():
@@ -89,24 +113,38 @@ def configure_servers():
 
 
 def main():
-    configure_servers()
 
-    # Create two subprocesses for the two nodes
-    #process1 = multiprocessing.Process(target=metadata_update, args=(NODES_1,))
-    #process2 = multiprocessing.Process(target=run_shell_script)
+    parser = argparse.ArgumentParser(description="Configure the servers")   
+    parser.add_argument('-t', '--test', action='store_true', help='Test parsing config', required=False)
+    parser.add_argument('-c', '--configure', action='store_true', help='Configure the servers', required=False)
+    parser.add_argument('-b', '--bench', action='store_true', help='Benchmakr the cluster', required=False)
 
-    #process2 = multiprocessing.Process(target=metadata_update, args=(NODES_2,))
-    #process3 = multiprocessing.Process(target=metadata_update, args=(NODES_3,))
+    args = parser.parse_args()
 
-    # Start the processes
-    #process1.start()
-    #process2.start()
-    #process3.start()
+    if args.test:
+        test_configurations()
+    elif args.configure:
+        configure_servers()
+    elif args.bench:
+        ip_list = get_ip_addresses()
+        # print(ip_list[0])
+        process1 = multiprocessing.Process(target=metadata_update, args=(["http://"+ip_list[0]+":8000"],))        
+        #process2 = multiprocessing.Process(target=run_shell_script)
 
-    # Wait for both processes to complete
-    #process1.join()
-    #process2.join()
-    #process3.join()
+        #process2 = multiprocessing.Process(target=metadata_update, args=(NODES_2,))
+        #process3 = multiprocessing.Process(target=metadata_update, args=(NODES_3,))
+
+        # Start the processes
+        process1.start()
+        #process2.start()
+        #process3.start()
+
+        # Wait for both processes to complete
+        process1.join()
+        #process2.join()
+        #process3.join()
+    else:
+        print("No arguments given")
 
 if __name__ == "__main__":
     main()
